4226d777ad0e742e676242c4e31b058790c05a2a
Un Épicier <=>
2023-05-04 11:10:57 +0200

    Fix missing variable

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const cors = require('cors')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const handlebars = require('express-handlebars')
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
 
 const hbs = handlebars.create({
-	defaultLayout: 'base',
-	extname: 'hbs',
-	encoding: 'utf-8',
-	layoutsDir: `./views/layouts`,
-	partialsDir: `./views/partials`,
-})
+  defaultLayout: "base",
+  extname: "hbs",
+  encoding: "utf-8",
+  layoutsDir: `./views/layouts`,
+  partialsDir: `./views/partials`,
+});
 
-app.use(cors())
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const port = 3000;
 
-app.set('view engine', 'hbs')
-app.engine('hbs', hbs.engine)
+app.use(cors());
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "hbs");
+app.engine("hbs", hbs.engine);
 
 // Routes
-app.use(require('./router'))
+app.use(require("./router"));
 
 app.listen(3000, () => {
-	console.log(`ClockJS started on http://localhost:${port}/`)
-})
+  console.log(`ClockJS started on http://localhost:${port}/`);
+});
